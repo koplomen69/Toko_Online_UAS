e208a89d01e63025a08bb0d0ed1c92cafd9135f2
Dedi <draconoiderd@gmail.com>
2024-06-08 19:12:33 +0700

    pull

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'foto',
+        'alamat',
+        'tlp',
+        'tglLahir',
+        'is_active',
+        'is_admin',
+        'is_mamber',
     ];
 
     /**
@@ -40,5 +47,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 }
